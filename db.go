0c6bb7b0faf69d48e37cde3cd2e4f05e85bba7d4
glen newton <glen.newton@gmail.com>
2016-12-31 18:13:38 -0500

    First commit

---
+++ db.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	//_ "github.com/mxk/go-sqlite"
+	//_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func dbOpen(dbFileName string) (*gorm.DB, error) {
+	db, err := gorm.Open("sqlite3", dbFileName)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	log.Println("Opening db file: ", dbFileName)
+
+	db.DB()
+	db.DB().Ping()
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+
+	sqlite3Config(db.DB())
+	return db, nil
+}
+
+func sqlite3Config(db *sql.DB) {
+	//db.Exec("PRAGMA auto_vacuum = 0;")
+	//db.Exec("PRAGMA cache_size=32768;")
+	db.Exec("PRAGMA cache_size=65536;")
+	db.Exec("PRAGMA count_changes = OFF;")
+	db.Exec("PRAGMA cache_spill = ON;")
+	//db.Exec("PRAGMA journal_size_limit = 67110000;")
+	db.Exec("PRAGMA locking_mode = EXCLUSIVE;")
+	//db.Exec("PRAGMA locking_mode = OFF;")
+	db.Exec("PRAGMA encoding = \"UTF-8\";")
+	//db.Exec("PRAGMA journal_mode = WAL;")
+
+	db.Exec("busy_timeout=0;")
+	db.Exec("legacy_file_format=OFF;")
+
+	//db.Exec("PRAGMA mmap_size=1099511627776;")
+	db.Exec("PRAGMA page_size = 40960;")
+
+	db.Exec("PRAGMA shrink_memory;")
+	db.Exec("PRAGMA synchronous=OFF;")
+	//db.Exec("PRAGMA synchronous = NORMAL;")
+	//db.Exec("PRAGMA temp_store = MEMORY;")
+	//db.Exec("PRAGMA threads = 5;")
+	//db.Exec("PRAGMA wal_autocheckpoint = 1638400;")
+}
+
+func dbInit(dbFile string) (*gorm.DB, error) {
+	db, err := dbOpen(dbFile)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	log.Printf("%v\n", *db)
+
+	db.CreateTable(&MeshTree{})
+	return db, nil
+}
+
+const table = "mesh_trees"
+
+func makeIndexes(db *gorm.DB) {
+	db.Table(table).AddIndex("descriptor_ui", "descriptor_ui")
+	db.Table(table).AddIndex("descriptor_name", "descriptor_name")
+
+	db.Table(table).AddIndex("tree", "tree")
+	db.Table(table).AddIndex("t0", "t0")
+	db.Table(table).AddIndex("t0_t1", "t0", "t1")
+	db.Table(table).AddIndex("t0_t1_t2", "t0", "t1", "t2")
+	db.Table(table).AddIndex("t0_t1_t2_t3", "t0", "t1", "t2", "t3")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4", "t0", "t1", "t2", "t3", "t4")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4_t5", "t0", "t1", "t2", "t3", "t4", "t5")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4_t5_t6", "t0", "t1", "t2", "t3", "t4", "t5", "t6")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4_t5_t6_t7", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4_t5_t6_t7_t8", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4_t5_t6_t7_t8_t9", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4_t5_t6_t7_t8_t9_t10", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10")
+	db.Table(table).AddIndex("t0_t1_t2_t3_t4_t5_t6_t7_t8_t9_t10_t11", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10", "t11")
+
+}
---
+++ main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gnewton/gomesh2016"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const DESC_FILE = "/home/gnewton/newtong/2014/mesh/2016/desc2016.bz2"
+
+var topLevel = []string{
+	"Anatomy", "A",
+	"Organisms", "B",
+	"Diseases", "C",
+	"Chemicals and Drugs", "D",
+	"Analytical, Diagnostic and Therapeutic Techniques and Equipment", "E",
+	"Psychiatry and Psychology", "F",
+	"Phenomena and Processes", "G",
+	"Disciplines and Occupations", "H",
+	"Anthropology, Education, Sociology and Social Phenomena", "I",
+	"Technology, Industry, Agriculture", "J",
+	"Humanities", "K",
+	"Information Science", "L",
+	"Named Groups", "M",
+	"Health Care", "N",
+	"Publication Characteristics", "V",
+	"Geographicals", "Z",
+}
+
+func main() {
+	fmt.Println(len(os.Args), os.Args)
+	loadData()
+
+}
+
+type MeshTree struct {
+	ID             int64
+	DescriptorUI   string `sql:"size:16"`
+	DescriptorName string
+	Tree           string
+	Depth          int
+	T0             *string `sql:"size:1"`
+	T1             *string `sql:"size:3"`
+	T2             *string `sql:"size:3"`
+	T3             *string `sql:"size:3"`
+	T4             *string `sql:"size:3"`
+	T5             *string `sql:"size:3"`
+	T6             *string `sql:"size:3"`
+	T7             *string `sql:"size:3"`
+	T8             *string `sql:"size:3"`
+	T9             *string `sql:"size:3"`
+	T10            *string `sql:"size:3"`
+	T11            *string `sql:"size:3"`
+	T12            *string `sql:"size:3"`
+}
+
+func loadData() {
+	db, err := dbInit("mesh2016_sqlite3.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var count int64 = 0
+	addToplevel(db, &count)
+
+	///////////////////////////
+	descChan, dfile, err := gomesh2016.DescriptorChannelFromFile(DESC_FILE)
+	defer dfile.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	depth := 0
+	dname := ""
+	ddes := ""
+	dtree := ""
+
+	for d := range descChan {
+		//fmt.Println(d)
+		//fmt.Println("[", d.DescriptorUI, d.DescriptorName, "]")
+		if d.DescriptorUI == "D005260" || d.DescriptorUI == "D008297" {
+			fmt.Printf("%+v\n", d)
+			//continue
+		}
+		if d.DescriptorUI == "D048531" {
+			fmt.Println(d)
+		}
+		if d.TreeNumberList != nil {
+			for _, tree := range d.TreeNumberList.TreeNumber {
+				count = count + 1
+				s, p := split(tree)
+				if d.DescriptorUI == "D048531" {
+					fmt.Println("+++", s, p)
+				}
+				mt := new(MeshTree)
+				mt.ID = count
+				mt.DescriptorUI = d.DescriptorUI
+				mt.DescriptorName = d.DescriptorName
+				mt.Tree = tree
+				mt.T0 = &s
+				l := len(p)
+				mt.Depth = l
+				switch l {
+
+				case 12:
+					mt.T12 = &p[11]
+					fmt.Println(tree)
+					fallthrough
+
+				case 11:
+					mt.T11 = &p[10]
+					fallthrough
+
+				case 10:
+					mt.T10 = &p[9]
+					fallthrough
+
+				case 9:
+					mt.T9 = &p[8]
+					fallthrough
+				case 8:
+					mt.T8 = &p[7]
+					fallthrough
+
+				case 7:
+					mt.T7 = &p[6]
+					fallthrough
+
+				case 6:
+					mt.T6 = &p[5]
+					fallthrough
+
+				case 5:
+					mt.T5 = &p[4]
+					fallthrough
+
+				case 4:
+					mt.T4 = &p[3]
+					fallthrough
+
+				case 3:
+					mt.T3 = &p[2]
+					fallthrough
+				case 2:
+					mt.T2 = &p[1]
+					fallthrough
+				case 1:
+					mt.T1 = &p[0]
+
+				}
+				db.Create(mt)
+
+				if len(p) > depth {
+					depth = len(p)
+					dname = d.DescriptorName
+					ddes = d.DescriptorUI
+					dtree = tree
+				}
+
+			}
+		}
+	}
+	fmt.Println(depth, dname, ddes, dtree)
+	makeIndexes(db)
+	db.Close()
+
+}
+
+func addToplevel(db *gorm.DB, count *int64) {
+
+	for i := 0; i < len(topLevel); i = i + 2 {
+		*count = *count + 1
+		fmt.Println(topLevel[i], topLevel[i+1])
+		name, id := topLevel[i], topLevel[i+1]
+		mt := new(MeshTree)
+		mt.ID = *count
+		mt.DescriptorUI = id
+		mt.DescriptorName = name
+		mt.Tree = id
+		mt.T0 = &id
+		db.Create(mt)
+	}
+}
+
+func split(t string) (string, []string) {
+	parts := strings.Split(t, ".")
+	start := string([]rune(t)[0])
+	first := parts[0]
+	parts[0] = string([]rune(first)[1:len(first)])
+	return start, parts
+}
